bec88c3dfae1cf590e8154d2d6be0a2334aaa747
Diego Bicelli <diegobicellim@gmail.com>
2024-04-09 16:28:19 -0300

    ajuste run.py

--- FastCrud/run.py
+++ FastCrud/run.py
@@ -5,12 +5,4 @@ from models import Item
 db_handle = DBConnectionHandler()
 db_handle.connect_to_db()
 db_connection = db_handle.get_db_connection()
-item_Repository = itemRepository(db_connection)
-
-documento = Item(
-    id="661071ec9e75dded8d9c1e0c",
-    name="Operação de Teste",
-    price=10
-)
-
-print(item_Repository.insert_document(documento))
+print(db_connection)
